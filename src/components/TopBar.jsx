1c4e723f231ae8b730bb9896ce04f126f8b1c435
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-09-10 09:37:01 +0100

    go

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,31 +1,39 @@
 export function TopBar() {
+  function printSpecificArea() {
+    window.print();
+  }
 
-    function printSpecificArea() {
-
-        window.print();
-    }
-
-    return (
-        <nav className={"navbar"}>
-            <div className={"brand-title"}>
-                <span className={"brand-logo"}><i className={"devicon-electron-original"}></i></span>
-                <span className={"brand-name"}>Resume Builder</span>
-            </div>
-            <a href={"#"} className={"toggle-button"}>
-                <span className={"bar"}></span>
-                <span className={"bar"}></span>
-                <span className={"bar"}></span>
-            </a>
-            <div className="navbar-links">
-                <ul>
-                    <li><a href={"#"}></a></li>
-                    <li><a href={"#"}></a></li>
-                    <li><a href={"#"}></a></li>
-                    <li><button className={"downloadButton"} onClick={printSpecificArea}>Download Resume</button></li>
-                </ul>
-            </div>
-        </nav>
-    )
-
+  return (
+    <nav className={'navbar'}>
+      <div className={'brand-title'}>
+        <span className={'brand-logo'}>
+          <i className={'devicon-electron-original'}></i>
+        </span>
+        <span className={'brand-name'}>RecruitAI</span>
+      </div>
+      <a href={'#'} className={'toggle-button'}>
+        <span className={'bar'}></span>
+        <span className={'bar'}></span>
+        <span className={'bar'}></span>
+      </a>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a href={'#'}></a>
+          </li>
+          <li>
+            <a href={'#'}></a>
+          </li>
+          <li>
+            <a href={'#'}></a>
+          </li>
+          <li>
+            <button className={'downloadButton'} onClick={printSpecificArea}>
+              Download Resume
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
-          
