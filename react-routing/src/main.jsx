4179b1364d489df91ee4992d53de723ebabc2c2b
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-09-23 21:44:24 +0100

    go

--- react-routing/src/main.jsx
+++ react-routing/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import Root from './routes/root';
+import ErrorPage from './error-page';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>
+    element: <Root />,
+    errorElement: <ErrorPage />
   }
 ]);
 
