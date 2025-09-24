a1f04ed18024aad30b43fddff721028b710ed869
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-09-24 09:15:00 +0100

    go

--- react-routing/src/routes/root.jsx
+++ react-routing/src/routes/root.jsx
@@ -1,4 +1,12 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from 'react-router-dom';
+import {
+  Outlet,
+  Link,
+  useLoaderData,
+  Form,
+  redirect,
+  NavLink,
+  useNavigation
+} from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
 export async function loader() {
@@ -38,16 +46,23 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
-                      <>
-                        {contact.first} {contact.last}
-                      </>
-                    ) : (
-                      <i>No Name</i>
-                    )}{' '}
-                    {contact.favorite && <span>★</span>}
-                  </Link>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{' '}
+                      {contact.favorite && <span>★</span>}
+                    </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
